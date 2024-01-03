4ab40c7f90a55250bdb9479e2ba5d188c15e9e2b
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-03 22:32:31 +0900

    저장완료시 메인페이지
    저장이 완료되면 메인페이지로 돌아가도록 구현했습니다.

--- frontend/write.js
+++ frontend/write.js
@@ -2,10 +2,24 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (event) => {
     event.preventDefault();
-    await fetch('/items', {
-        method: 'POST',
-        body: new FormData(form)
-    })
+
+    try {
+        const res = await fetch('/items', {
+            method: 'POST',
+            body: new FormData(form)
+        });
+
+        const data = await res.json();
+        if (data === '200') window.location.pathname = "/";
+    } catch (e) {
+        console.error('이미지 업로드 실패');
+    }
+
+    // 성공하면 메인페이지로
+    // const data = await res.json();
+    // if (data === '200') window.location.pathname = "/";
+    // else console.error('이미지 업로드 실패');
 }
 
 form.addEventListener('submit', handleSubmitForm)
+
