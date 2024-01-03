7cdcdfcb5aa564ce0b11e26093a53cae52a7fc0c
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-04 01:24:39 +0900

    시간 표시 및 최신글 위로 정렬
    게시글이 등록된 시간과 현재 시간을 계산해서 얼마 전의 글인지 시간 표시해주는 기능을 구현하고, 최신글일수록 위에 있도록 정렬된 화면 구현했습니다.

--- frontend/index.js
+++ frontend/index.js
@@ -11,10 +11,25 @@
 </div> 
 */}
 
+const calcTime = (timestamp) => {
+    // const curTime = new Date().getTime(); // 한국시간 (UTC + 9)
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000; // 세계시간으로 맞춰주기
+    const time = new Date(curTime - timestamp);
+    const hour = time.getHours();
+    const minutes = time.getMinutes();
+    const second = time.getSeconds();
+
+    if (hour > 0) return `${hour}시간 전`;
+    else if (minutes > 0) return `${minutes}분 전`;
+    else if (second >= 0) return `${second}초 전`;
+    else '방금 전';
+}
+
 const renderData = (data) => {
     const main = document.querySelector("main.item-tap");
 
-    data.forEach((obj) => {
+    // 최신글이 위로 올라오도록 reverse 사용
+    data.reverse().forEach((obj) => {
         const div = document.createElement("div");
         div.className = "item-list";
 
@@ -33,7 +48,7 @@ const renderData = (data) => {
 
         const InfoMetaDiv = document.createElement("div");
         InfoMetaDiv.className = "item-list__info-meta";
-        InfoMetaDiv.innerText = obj.place
+        InfoMetaDiv.innerText = obj.place + ' ' + calcTime(obj.insertAt); // 장소 + 시간 출력
 
         const InfoPriceDiv = document.createElement("div");
         InfoPriceDiv.className = "item-list__info-price";
