7d1c00f06d4da2446fd2736895637da4e34aca4e
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-05 20:52:29 +0900

    Svelte 사용
    Svelte로 당근마켓 클론코딩

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  // 모듈 방식
+  import Main from "./pages/Main.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Login from "./pages/Login.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    '/': Main,
+    '/signup': Signup,
+    '/login': Login,
+    '/Write': Write,
+    '*': NotFound // 그 외 모든 페이지 (잘못된 경로)
+  }
+  
+</script>
+
+<Router routes={routes} />
+
+<style>
+  div {
+    color: red;
+  }
+</style>
